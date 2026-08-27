b51be2291f332ea8bc804139d1706c8a5627d8ac
vetohkinalmalik-web <vetohkinalmalik@gmail.com>
2026-08-27 19:02:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -46,9 +46,10 @@ INDEX_PATH = BASE_DIR / "index.html"
 ADMIN_INDEX_PATH = BASE_DIR / "admin.html"
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8075824870:AAFyv88pbJJ8FdhkXPWskQFju8bYoZB1RRo").strip()
-
-try:
 ADMIN_ID = str(os.getenv("ADMIN_ID", "802560745")).strip()
+
+INIT_DATA_MAX_AGE = 300
+RATE_WINDOW = 10
 
 INIT_DATA_MAX_AGE = 300
 RATE_WINDOW = 10
